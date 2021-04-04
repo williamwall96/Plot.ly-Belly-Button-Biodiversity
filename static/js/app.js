953c5578b66569da212617b7663a802b8c30e243
Will Walles <willwalles@Wills-MacBook-Pro.local>
2021-04-04 16:47:06 -0500

    function to build the demographic info panel was made

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,19 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("static/js/data/samples.json").then((importedData) => {
-    console.log(importedData);
-    var data = importedData;
-    // define some key variables
-    var nameList =  data.names;
-    var meta = data.metadata;
-    console.log(meta);
-    var samples = data.samples;
-    console.log(samples);
+// function to build the Demographic Info Panel
 
-    // function filterSamples(sample) {
-    //     sample.id === "940";
-    // };
-
-    // var id940 = filterSamples(samples);
-
-    dropDownMenu = d3.selectAll("#selDataset"); 
-    // map the nameList so that the dropdown menu has the IDs
-    nameList.map((item) => {
-        option = dropDownMenu.append("option");
-        option.text(item);
+function buildMetadata(sample) {
+    d3.json("static/js/data/samples.json").then((data) => {
+      var metadata= data.metadata;
+      var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
+      var result= resultsarray[0]
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key}: ${value}`);
+      });
     });
+  };
 
-});
+
 
 
 
